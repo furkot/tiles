9a114a447a15cadd8b548394138f49ec13fa80d0
Natalia Kowalczyk <melitele@code42day.com>
2017-04-07 17:01:24 -0700

    support image tiles

--- test/tiles.js
+++ test/tiles.js
@@ -1,9 +1,10 @@
-var should = require('should');
 var furkotTiles = require('../lib/tiles');
 
 describe('furkot-tiles node module', function () {
-  it('must have at least one test', function () {
-    furkotTiles();
-    should.fail('Need to write tests.');
+  it('image', function () {
+    var tiles = furkotTiles({
+      input: 'png'
+    });
+    tiles.should.have.property('getTileUrl');
   });
 });
