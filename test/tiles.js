c4ef57c3c897b7aea70d4bfb9f1fbde3a9007fa2
Natalia Kowalczyk <melitele@code42day.com>
2017-04-09 18:56:08 -0700

    render vector tiles as svg/path

--- test/tiles.js
+++ test/tiles.js
@@ -7,4 +7,15 @@ describe('furkot-tiles node module', function () {
     });
     tiles.should.have.property('getTileUrl');
   });
+
+  it('topjson -> svg', function () {
+    var tiles = furkotTiles({
+      service: 'mapzen',
+      input: 'topojson',
+      output: 'svg',
+      tileSize: [512, 256]
+    });
+    tiles.should.have.property('getTile');
+    tiles.should.have.property('releaseTile');
+  });
 });
